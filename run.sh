952244db0f57d42e7ca50ef453bda825091aa9c9
qzwlecr <qzwlecr@gmail.com>
2017-12-31 20:24:09 +0800

    Finish

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sbt package
-mv ~/Documents/cloud-computing-skill-competition/target/scala-2.11/association_rules_2.11-1.0.jar ~/Documents/cloud-computing-skill-competition/target/scala-2.11/association_rules.jar
-scp ~/Documents/cloud-computing-skill-competition/target/scala-2.11/association_rules.jar root@router:
-ssh root@router scp association_rules.jar node307:
-ssh -t root@router ssh -t node307 screen ./run.sh AR.Main association_rules.jar
--- run_in.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sbt package
-mv ~/Documents/cloud-computing-skill-competition/target/scala-2.11/association_rules_2.11-1.0.jar ~/Documents/cloud-computing-skill-competition/target/scala-2.11/association_rules.jar
-scp ~/Documents/cloud-computing-skill-competition/target/scala-2.11/association_rules.jar uniquesc@202.114.10.172:
-ssh -t uniquesc@202.114.10.172 screen ./run.sh AR.Main association_rules.jar
