87742da82320842ef47d4d3736baa80be9aa92f3
FluorineDog <fluorine@gmail.com>
2018-04-15 17:06:40 +0800

    init

---
+++ fuck.sh
@@ -0,0 +1,4 @@
+# sbt package && \
+
+scp target/scala-2.11/association_rules_2.11-1.0.jar ccp:~/dog-testing/ar.jar &&
+ssh ccp "cd dog-testing; pwd; ./run.sh AR.Test ar.jar"
---
+++ src/main/scala/Test.scala
@@ -0,0 +1,43 @@
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+package AR {
+
+  object Test {
+    def main(args: Array[String]): Unit = {
+      val fileInput = args(0)
+      val fileOutput = args(1)
+      val fileTemp = args(2)
+      val conf = new SparkConf().setAppName("Association Rules")
+        .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+        .set("spark.network.timeout", "3000")
+      val sc = new SparkContext(conf)
+      val originData = sc.textFile(fileInput + "/D.dat", 1800)
+      val transactionsFull: RDD[Array[Int]] = originData.map(s => s.trim.split(' ').map(x => x.toInt))
+      val transactions = transactionsFull.sample(false, 0.01, 810L).cache()
+      val model = new FPGrowth().setMinSupport(0.092).setNumPartitions(900).run(transactions)
+
+      model.freqItemsets.map(x => x.items.reverse.mkString(" ")).sortBy(x => x).saveAsTextFile(fileOutput + "/freqList_sample_std.dat")
+//      val answerData = sc.textFile(fileInput + "/D-answer.dat").map(x=>x.trim.split(" ").map(x=>x.toInt)).map(x=>(x.take(x.length-1),x.last))
+//      val freqItemss = sc.textFile(fileInput + "/freq.dat").collect().map(x=>x.trim.split(" ").map(x=>x.toInt))
+//      val freqItems = freqItemss(0)
+//      val itemsWithFreq = model.freqItemsets.map(x => (x.items.toList, x.freq)).collect()
+//      val itemsWithFreqMap = itemsWithFreq.toMap
+//      var root: RulesTree = RuleNode(0, 0.0, Nil)
+//      for ((items, son) <- itemsWithFreq) {
+//        if (items.length > 1) {
+//          items.foreach(
+//            x => {
+//              val mother = items diff List(x)
+//              root = root.insert(mother, x, 1.0 * son / itemsWithFreqMap(mother))
+//            }
+//          )
+//        }
+//      }
+//      val tree = sc.broadcast(root)
+//      val userData = sc.textFile(fileInput + "/U.dat", 300)
+//      val users = userData.map(s => s.trim.split(' ').map(x => x.toInt)).map(x => x.intersect(model.freqItems))
+//      users.map(x => tree.value.find(x.toSet)._1).saveAsTextFile(fileOutput + "/U.dat")
+    }
+  }
+
+}
